36255d8f0942aa505945dd189378684aaf622f0e
Saman Miran <c@samanmiran.com>
2023-04-08 23:24:25 +0200

    Title

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,18 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
 #include <iostream>
 
 #include "application.h"
 
 int main() {
     Application app{};
+    app.title = "Hello World!";
 
     try {
         app.run();
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
