e036e4b3718a9efecf88cae758722eaa2a9eb54a
Saman Miran <c@samanmiran.com>
2023-04-11 16:10:58 +0200

    Purple triangle

--- include/triangle.h
+++ include/triangle.h
@@ -11,9 +11,9 @@ class Triangle {
 public:
     Renderable renderable{{
                                   Vertex{{0.0f, -0.5f, 0.0f},
-                                         {1.0f, 0.0f,  0.0f}},
+                                         {1.0f, 1.0f,  1.0f}},
                                   Vertex{{0.5f, 0.5f, 0.0f},
-                                         {0.0f, 1.0f, 0.0f}},
+                                         {1.0f, 0.0f, 0.0f}},
                                   Vertex{{-0.5f, 0.5f, 0.0f},
                                          {0.0f,  0.0f, 1.0f}}
                           }};
