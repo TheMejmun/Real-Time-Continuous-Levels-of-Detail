f39ff0a288b81ad8b34cedbfc00463c678d64e21
Saman Miran <c@samanmiran.com>
2023-04-18 13:45:05 +0200

    Use actual max resolution in full screen

--- source/window_manager.cpp
+++ source/window_manager.cpp
@@ -6,68 +6,78 @@
 #include "window_manager.h"
 #include "printer.h"
 
-void WindowManager::create(const std::string& t) {
-	INF "Creating WindowManager" ENDL;
+void WindowManager::create(const std::string &t) {
+    INF "Creating WindowManager" ENDL;
 
-	this->title = t;
+    this->title = t;
 
-	glfwInit();
+    glfwInit();
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-	this->window = glfwCreateWindow(this->width, this->height, this->title.c_str(), nullptr, nullptr);
-	this->monitor = glfwGetPrimaryMonitor();
-	pollMonitorResolution();
+    this->window = glfwCreateWindow(this->width, this->height, this->title.c_str(), nullptr, nullptr);
+    this->monitor = glfwGetPrimaryMonitor();
+    pollMonitorResolution();
 }
 
-void WindowManager::updateTitle(const std::string& t) {
-	this->title = t;
-	if (this->window != nullptr) {
-		glfwSetWindowTitle(this->window, this->title.c_str());
-	}
+void WindowManager::updateTitle(const std::string &t) {
+    this->title = t;
+    if (this->window != nullptr) {
+        glfwSetWindowTitle(this->window, this->title.c_str());
+    }
 }
 
 bool WindowManager::shouldClose() const {
-	return glfwWindowShouldClose(this->window);
+    return glfwWindowShouldClose(this->window);
 }
 
 void WindowManager::close() const {
-	glfwSetWindowShouldClose(this->window, GLFW_TRUE);
+    glfwSetWindowShouldClose(this->window, GLFW_TRUE);
 }
 
 void WindowManager::toggleFullscreen() {
-	this->isMaximized = !this->isMaximized;
-	if (this->isMaximized) {
-		pollWindowPosition(); // For restoring later
-		glfwSetWindowMonitor(this->window, glfwGetPrimaryMonitor(), 0, 0, this->monitorParams->width, this->monitorParams->height, GLFW_DONT_CARE);
-	}
-	else {
-		glfwSetWindowMonitor(this->window, nullptr,
-			this->windowPosX, this->windowPosY,
-			this->width, this->height, GLFW_DONT_CARE);
-	}
+    this->isMaximized = !this->isMaximized;
+    if (this->isMaximized) {
+        pollWindowPosition(); // For restoring later
+        glfwSetWindowMonitor(this->window, glfwGetPrimaryMonitor(),
+                             0, 0,
+                             this->monitorParams->width, this->monitorParams->height,
+                             this->monitorParams->refreshRate);
+    } else {
+        glfwSetWindowMonitor(this->window, nullptr,
+                             this->windowPosX, this->windowPosY,
+                             this->width, this->height, GLFW_DONT_CARE);
+    }
 }
 
+
 void WindowManager::pollMonitorResolution() {
-	this->monitorParams = glfwGetVideoMode(this->monitor);
-	DBG "Got monitor info:" <<
-		"\n\tWidth:\t" << this->monitorParams->width <<
-		"\n\tHeight:\t" << this->monitorParams->height <<
-		"\n\tRed Bits:\t" << this->monitorParams->redBits <<
-		"\n\tGreen Bits:\t" << this->monitorParams->greenBits <<
-		"\n\tBlue Bits:\t" << this->monitorParams->blueBits <<
-		"\n\tHz:\t" << this->monitorParams->refreshRate ENDL;
+    // Supposed to be valid until monitor disconnected, but seems to change between fullscreen and windowed
+    this->monitorParams = glfwGetVideoMode(this->monitor);
+    int count;
+    auto params = glfwGetVideoModes(this->monitor, &count);
+
+    for (int i = 0; i < count; ++i) {
+        DBG "Got monitor info:" <<
+                                "\n\tWidth:\t" << params[i].width <<
+                                "\n\tHeight:\t" << params[i].height <<
+                                "\n\tRed Bits:\t" << params[i].redBits <<
+                                "\n\tGreen Bits:\t" << params[i].greenBits <<
+                                "\n\tBlue Bits:\t" << params[i].blueBits <<
+                                "\n\tHz:\t" << params[i].refreshRate ENDL;
+        this->monitorParams = &params[i];
+    }
 }
 
 void WindowManager::pollWindowPosition() {
-	glfwGetWindowPos(this->window, &this->windowPosX, &this->windowPosY);
+    glfwGetWindowPos(this->window, &this->windowPosX, &this->windowPosY);
 }
 
 void WindowManager::destroy() const {
-	INF "Destroying WindowManager" ENDL;
+    INF "Destroying WindowManager" ENDL;
 
-	glfwDestroyWindow(this->window);
+    glfwDestroyWindow(this->window);
 
-	glfwTerminate();
+    glfwTerminate();
 }
