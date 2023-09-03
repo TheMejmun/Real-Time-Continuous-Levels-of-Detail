e142b2a362e3d86d8a1752cd87967fb6f41ea8ce
Saman Miran <c@samanmiran.com>
2023-09-03 14:59:53 +0200

    Log duration 60s

--- source/util/performance_logging.cpp
+++ source/util/performance_logging.cpp
@@ -10,7 +10,7 @@
 #include <sys/stat.h>
 #include <sstream>
 
-extern const sec PerformanceLogging::LOG_DURATION = 30.0;
+extern const sec PerformanceLogging::LOG_DURATION = 60.0;
 const uint32_t EXPECTED_MAX_FRAME_COUNT =
         static_cast<uint32_t>(PerformanceLogging::LOG_DURATION) * 2000; // seconds * frames
 
