f6965c57a686f8e54019ebfc2df6a236f676a401
Saman Miran <c@samanmiran.com>
2023-09-03 14:51:29 +0200

    Sphere at 100% screen height

--- source/ecs/entities/camera.cpp
+++ source/ecs/entities/camera.cpp
@@ -6,10 +6,12 @@
 #include "util/importer.h"
 #include "graphics/colors.h"
 
+#define DISTANCE_WITH_100_PERCENT_SCREEN_HEIGHT_SPHERE (-2.65)
+
 Camera::Camera() {
     this->components.transform = std::make_unique<Transformer4>();
-    this->components.transform->translate(glm::vec3(0, 0, -1.5));
-//    this->components.transform->translate(glm::vec3(0, 0, -3));
-//    this->components.transform->translate(glm::vec3(0, 0, -30));
+//    this->components.transform->translate(glm::vec3(0, 0, DISTANCE_WITH_100_PERCENT_SCREEN_HEIGHT_SPHERE / 2));
+    this->components.transform->translate(glm::vec3(0, 0, DISTANCE_WITH_100_PERCENT_SCREEN_HEIGHT_SPHERE));
+//    this->components.transform->translate(glm::vec3(0, 0, DISTANCE_WITH_100_PERCENT_SCREEN_HEIGHT_SPHERE * 10));
     this->components.camera = std::make_unique<Projector>();
 }
